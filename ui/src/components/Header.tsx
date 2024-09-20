0f6d701477330d47eea06ec276eb922c9839876b
lemorage <one.lemorage@gmail.com>
2024-09-20 22:08:44 +0800

    feat: Add and update logo file in Header component

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -1,19 +1,21 @@
-// Header.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 text-white py-4 px-6">
+    <header className="bg-black text-white py-4 px-6">
       <div className="container mx-auto flex justify-between items-center">
-        <div>
-          <Link to="/" className="text-2xl font-semibold text-white">My Website Logo</Link>
+        <div className="flex items-center">
+          <Link to="/" className="text-2xl font-semibold text-white">
+            <img src="/silveriff.png" alt="Silveriff Logo" className="h-48 ml-0 pl-0 mr-4" />
+          </Link>
         </div>
-        <nav className="space-x-4">
-          <Link to="/" className="text-white hover:underline">Home</Link>
-          <Link to="/projects" className="text-white hover:underline">Projects</Link>
-          <Link to="/service" className="text-white hover:underline">Service</Link>
-          <Link to="/contact" className="text-white hover:underline">Contact</Link>
+        
+        <nav className="flex bg-black space-x-12 justify-end items-center">
+          <Link to="/" className="text-white hover:underline uppercase">Home</Link>
+          <Link to="/projects" className="text-white hover:underline uppercase">Projects</Link>
+          <Link to="/service" className="text-white hover:underline uppercase">About</Link>
+          <Link to="/contact" className="text-white hover:bg-gray-600 px-4 py-2 rounded uppercase">Contact Us</Link>
         </nav>
       </div>
     </header>
