dc4732f6599256bb1f7c8b531a51a775a44f42a5
lemorage <one.lemorage@gmail.com>
2024-09-21 22:55:30 +0800

    refactor: Update Footer component layout and styling

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -1,23 +1,20 @@
-// Footer.tsx
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 px-6 mt-auto">
+    <footer className="bg-black text-white py-8 px-6">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
-          <div className="mr-4">
-            <a href="tel:+8616628718335" className="text-white hover:underline">(+86)16628718335</a>
-          </div>
-          <div>
-            <Link to="/" className="text-2xl font-semibold text-white">My Website Logo</Link>
-          </div>
+        {/* Left Section: Contact Information */}
+        <div className="flex flex-col text-left">
+          <p className="mb-2">Phone: <a href="tel:+8616628718335" className="hover:underline">(+86) 166 2871 8335</a></p>
+          <p>Email: <a href="mailto:contact@silveriff.com" className="hover:underline">contact@silveriff.com</a></p>
         </div>
-        <div>
-          <a href="https://www.instagram.com" className="mr-4 text-white hover:underline">Instagram</a>
-          <a href="https://www.linkedin.com" className="mr-4 text-white hover:underline">LinkedIn</a>
-          <a href="mailto:contact@silveriff.com" className="text-white hover:underline">Email</a>
+
+        {/* Right Section: Logo */}
+        <div className="text-center">
+          <a href="/" className="text-2xl font-semibold hover:underline" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+            <img src="/silveriff.png" alt="Silveriff Logo" className="h-12 mx-auto" />
+          </a>
         </div>
       </div>
     </footer>
